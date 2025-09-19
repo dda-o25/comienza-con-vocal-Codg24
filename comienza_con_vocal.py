8970614378c3e00bd35a2eced23a5a799e16cefc
Codg24 <carlos.diaz@iteso.mx>
2025-09-19 02:29:30 -0600

    Quinta solución

--- comienza_con_vocal.py
+++ comienza_con_vocal.py
@@ -16,8 +16,6 @@ letra = palabra[0:1].lower()
 if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u" or letra == "á" or letra == "é" or letra == "í" or letra == "ó" or letra == "ú" or letra == "ü":
     # Salida
     print(f"'{palabra}' comienza con vocal")
-
-    palabra
 else:
     #Salida
     print(f"'{palabra}' no comienza con vocal")
