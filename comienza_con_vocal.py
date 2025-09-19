9205cb46db4f4145a1d8fc19c2f0dc174c659a8f
Codg24 <carlos.diaz@iteso.mx>
2025-09-18 23:53:24 -0600

    Primera solución

--- comienza_con_vocal.py
+++ comienza_con_vocal.py
@@ -1,15 +1,22 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Integrantes
+Carlos Osvaldo Díaz García
+
+Fecha
+18 de septiembre de 2025
+
+Determinar si una palabra comienza con una vocal.
 """
 
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+palabra = input("Escribe una palabra: ")
 
 # Proceso
+letra = palabra[0:1].lower()
 
-
-# Salidas
-print(salida)
+if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "á" or letra == "é" or letra == "í" or letra == "ó" or letra == "ú":
+    # Salida
+    print(palabra, "comienza con vocal")
+else:
+    #Salida
+    print(palabra, "no comienza con vocal")
